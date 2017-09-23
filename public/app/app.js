6c582bf718c39cc12d16417a415ce7458fbb2e9e
Amol Wankhede <amolsw.nz@gmail.com>
2017-09-23 15:21:56 +1200

    Event scope and controller

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,7 @@
   'use strict';
   angular.module('mainApp', [])
     .controller('mainCtrl', ['$scope', function($scope) {
-      $scope.userData = {
+      $scope.user1 = {
         "id": "88712433",
         "name": "Candace Nicolas",
         "phone": "(591) 414-5492",
@@ -10,34 +10,97 @@
           "street": "847 Clare Rapids",
           "city": "Uptonville",
           "state": "Hawaii"
+        },
+        "friends": [
+          'Apple',
+          'Mango',
+          'Orange',
+          'Lemon'
+        ]
+      };
+
+      $scope.user2 = {
+        "name": "Mr. Ines Wehner",
+        "phone": "(234) 830-1931",
+        "address": {
+          "street": "11433 Carolanne Fall",
+          "city": "South Jaylanside",
+          "state": "Missouri"
         }
       };
+
     }])
+
     .directive('userInfoCard', function() {
       return {
         templateUrl: "template/userInfoCard.html",
-        restrict: 'E'
+        restrict: 'E',
+        scope: {
+          user: '=',
+          initialCollapsed: '@collapsed'
+        },
+        controller: function($scope) {
+          $scope.collapsed = ($scope.initialCollapsed === 'true');
+          $scope.knightMe = function(user) {
+            console.log('clicked');
+            $scope.user.rank = 'knight';
+          };
+          $scope.collapse = function() {
+            $scope.collapsed = !$scope.collapsed;
+          };
+          $scope.removeFriend = function(friend) {
+            console.log('clicked', friend);
+            var id = $scope.user.friends.indexOf(friend);
+            console.log(id);
+            if (id > -1)
+              $scope.user.friends.splice(id, 1);
+          };
+        }
       };
-    });
+    })
+
+    .directive('userAddress', function() {
+      return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'template/userAddress.html',
+        controller: function($scope) {
+          $scope.collapsed = false;
+          $scope.collapseAdd = function() {
+            $scope.collapsed = !$scope.collapsed;
+          };
+          $scope.explandAdd = function() {
+            $scope.collapsed = !$scope.collapsed;
+          };
+        }
+      };
+    })
+
+    .directive('removeFriend', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'template/removeFriend.html',
+        scope: {
+          notifyParent: '&method'
+        },
+        controller: function($scope) {
+          $scope.removing = false;
+          $scope.removingThis = function() {
+            $scope.removing = !$scope.removing;
+          };
+          $scope.cancelRemove = function() {
+            $scope.removing = !$scope.removing;
+          };
+
+          $scope.confirmRemove = function() {
+            $scope.notifyParent();
+          };
+
+        }
+      };
+    })
+
+
+
+  ;
 }());
-// (function() {
-//
-//   app.config(function($routeProvider) {
-//     $routeProvider
-//       .when("/main", {
-//         templateUrl: "main.html",
-//         controller: "MainController"
-//       })
-//       .when("/user/:userName", {
-//         templateUrl: "userDetails.html",
-//         controller: "UserController"
-//       })
-//       .otherwise({
-//         // template : "<h1>None</h1><p>Nothing has been selected,</p>"
-//         redirectTo: "main"
-//       });
-//   });
-//
-//   });
-//
-// }());
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # In depth AngularJS Directives
+* Binding
+* Templates
+* Adding Controller to Directives
+* Directive Scopes
+* Scope and Multiple Directives
+* Inherited Scopes
+  * Simple Value Parameters
+  * Function Parameters
